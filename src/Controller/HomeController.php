0149e30245e4cf7e3f300b296f46c13b3f777dbf
BASAK Semih <semihbasak25@gmail.com>
2022-03-30 15:16:15 +0200

    User can see hotel

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\HotelRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,11 @@ use Symfony\Component\Routing\Annotation\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'homePage')]
-    public function home(): Response
+    public function home(HotelRepository $hotelRepository): Response
     {
-        return $this->render('home/home.html.twig');
+        $hotels = $hotelRepository->findAll();
+        return $this->render('home/home.html.twig', [
+            'hotels' => $hotels
+        ]);
     }
 }
