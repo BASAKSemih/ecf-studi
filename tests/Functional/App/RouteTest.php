46a6c96bdab902861857b9765ae1c492e3fe3ff9
BASAK Semih <semihbasak25@gmail.com>
2022-03-30 15:20:20 +0200

    fix twig

--- tests/Functional/App/RouteTest.php
+++ tests/Functional/App/RouteTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional\App;
 
+use App\Entity\Hotel;
+use App\Entity\Manager;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
@@ -18,4 +20,18 @@ final class RouteTest extends WebTestCase
         $crawler = $client->request(Request::METHOD_GET, $router->generate('homePage'));
         self::assertRouteSame('homePage');
     }
+    public function testShowHotelById(): void
+    {
+        $client = static::createClient();
+        /** @var RouterInterface $router */
+        $router = $client->getContainer()->get('router');
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+        $hotelRepository = $entityManager->getRepository(Hotel::class);
+        $hotel = $hotelRepository->findOneById(1);
+        $crawler = $client->request(Request::METHOD_GET, $router->generate('user_show_hotel', [
+            'idHotel' => $hotel->getId()
+        ]));
+        self::assertRouteSame('user_show_hotel');
+    }
+
 }
