70391bddc8947ab3bac882a34cb64194cffb7362
BASAK Semih <semihbasak25@gmail.com>
2022-04-21 11:10:12 +0200

    feat: fix twig

--- src/Controller/Manager/ManagerController.php
+++ src/Controller/Manager/ManagerController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller\Manager;
 
 use App\Entity\Hotel;
+use App\Entity\Manager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,12 +16,15 @@ final class ManagerController extends AbstractController
     #[Route('/espace-manager/', name: 'homePage')]
     public function homePage(): Response
     {
+        /** @var Manager $manager */
+        $manager = $this->getUser();
         /** @var Hotel $hotel */
-        $hotel = $this->getUser()->getHotel();
+        $hotel = $manager->getHotel();
         $rooms = $hotel->getRooms();
+
         return $this->render('manager/home/index.html.twig', [
             'hotel' => $hotel,
-            'rooms' => $rooms
+            'rooms' => $rooms,
         ]);
     }
 }
